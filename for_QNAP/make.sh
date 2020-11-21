59fd2e9534b89da7ae47a8f706a15882a2485161
wataru <wataru775@gmail.com>
2020-11-21 10:54:49 +0900

    QNAPのイメージの作成

---
+++ for_QNAP/make.sh
@@ -0,0 +1,56 @@
+##
+#  これはQNAPのcontainer stationで端末実行内で実行する作業です
+#  検証済コンテナ : ubuntu:latest (20.04.1 LTS)
+##
+
+apt-get update
+apt-get upgrade
+
+apt-get -y install php php-fpm unzip mcrypt php-fpm php-common php-mbstring php-xmlrpc php-gd php-xml php-mysql php-cli php-zip php-curl php-imagick git curl rsync
+
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# for apache2
+sed -e 's/APACHE_RUN_USER=www-data/APACHE_RUN_USER=gitlab-runner/g' /etc/apache2/envvars
+# sed -e 's/APACHE_RUN_USER=www-data/APACHE_RUN_USER=gitlab-runner/g' /etc/apache2/envvars
+
+chown gitlab-runner:www-data /etc/apache2/sites-available/000-default.conf
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        # DocumentRoot /var/www/html
+        DocumentRoot /opt/runner/public
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        <Directory /opt/runner/public>
+                AllowOverride All
+                Options Includes FollowSymLinks
+                Require all granted
+        </Directory>
+</VirtualHost>
+EOF
+
+chown -R gitlab-runner:www-data /var/log/apache2
+
+# working / www
+mkdir /opt/runner/
+chown gitlab-runner:www-data /opt/runner/
+chmod 2777 /opt/runner/
+
+# for php
+php -v
+
+
+# for composer
+curl https://getcomposer.org/composer-stable.phar -o /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+composer -V
+
+apache2ctl configtest
+service apache2 restart
+
+curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+chmod +x /usr/local/bin/gitlab-runner
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+gitlab-runner start
